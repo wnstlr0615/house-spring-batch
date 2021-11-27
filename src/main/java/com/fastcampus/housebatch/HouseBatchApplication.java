bce7ebbfc15a50fd1c8652dc768e77f5335dde91
joonsik <ryan0@kakao.com>
2021-11-27 22:38:29 +0900

    application.yml 파일 생성
    프로파일 및 데이터소스 설정

--- src/main/java/com/fastcampus/housebatch/HouseBatchApplication.java
+++ src/main/java/com/fastcampus/housebatch/HouseBatchApplication.java
@@ -1,8 +1,12 @@
 package com.fastcampus.housebatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.util.Assert;
+@EnableBatchProcessing
+@EnableJpaAuditing
 @SpringBootApplication
 public class HouseBatchApplication {
 
