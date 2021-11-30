379682e778ae7394940cd08e0fce6721c0178986
joonsik <ryan0@kakao.com>
2021-11-30 20:13:49 +0900

    Update LawdRepository
    법정동코드에서 구코드 가져오는 쿼리 추가

--- src/main/java/com/fastcampus/housebatch/core/repository/LawdRepository.java
+++ src/main/java/com/fastcampus/housebatch/core/repository/LawdRepository.java
@@ -2,9 +2,13 @@ package com.fastcampus.housebatch.core.repository;
 
 import com.fastcampus.housebatch.core.domain.Lawd;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LawdRepository extends JpaRepository<Lawd, Long> {
     Optional<Lawd> findByLawdCd(String lawdCd);
+    @Query(value = "select distinct substring(l.lawdCd, 1, 5)  from Lawd as l  where l.exist=true  and l.lawdCd  not like '%00000000'")
+    List<String> findDistinctGuLawdCd();
 }
